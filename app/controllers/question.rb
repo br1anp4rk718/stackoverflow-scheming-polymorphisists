ceb333a5241d19ce03295003eb54095a46a5a736
br1anp4rk <br1anp4rk718@gmail.com>
2015-09-16 14:52:42 -0700

    css layout first draft and homepage route

---
+++ app/controllers/question.rb
@@ -0,0 +1,41 @@
+get '/questions' do
+	# @questions = Question.all
+	erb :index
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 	get	'/questions' do         # display a list of all #{resource}
+# 	end
+
+# 	get	'/#{resource}/new' do	    # return an html form for creating a new photo
+# 	end
+
+# 	post '/#{resource}' do        # create a new resource
+# 	end
+
+# 	get	'/#{resource}/:id' do	    # display a specific resource
+# 	end
+
+# 	get	'/#{resource}/:id/edit' do# return an html form for editing a resource
+# 	end
+
+# 	put	'/#{resource}/:id' do     # update a specific resource
+# 	end
+
+# 	delete	'/#{resource}/:id' do # delete a specific resource
+# 	end
+
+# 	RESTFUL_ROUTES
+# end
