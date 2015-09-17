0025a2a2defe0e4750571b4fbea0dac0987a4ab0
davidbison <davidbisonhubert@gmail.com>
2015-09-16 19:05:22 -0700

    Add association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   include BCrypt
   has_many :questions, dependent: :destroy
-  has_many :answers, dependent: :destroy
+  has_many :answers, through: :questions, dependent: :destroy
   has_many :responses, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
